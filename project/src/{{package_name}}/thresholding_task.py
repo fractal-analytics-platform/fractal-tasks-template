81ecdc53e29d4f2fafe67d4b714c77d1a9873fd8
jluethi <joel.luethi@uzh.ch>
2024-01-15 17:19:44 +0100

    Update imports

--- project/src/{{package_name}}/thresholding_task.py
+++ project/src/{{package_name}}/thresholding_task.py
@@ -11,8 +11,8 @@ import dask.array as da
 
 from pydantic.decorator import validate_arguments
 
-from fractal_tasks_core.lib_ngff import load_NgffImageMeta
-from fractal_tasks_core.lib_pyramid_creation import build_pyramid
+from fractal_tasks_core.ngff import load_NgffImageMeta
+from fractal_tasks_core.pyramids import build_pyramid
 
 @validate_arguments
 def thresholding_task(
