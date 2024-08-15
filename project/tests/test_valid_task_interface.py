090f8035f9635cb9a5a6f70ac46f42e1674f0a52
lorenzo <lorenzo.cerrone@uzh.ch>
2024-08-15 17:23:44 +0200

    fix Update tests for pydantic v2: `validate_command` #13 + other optimize imports

--- project/tests/test_valid_task_interface.py
+++ project/tests/test_valid_task_interface.py
@@ -1,13 +1,11 @@
 import json
 import subprocess
 from shlex import split as shlex_split
-from subprocess import PIPE
 
 import pytest
 from devtools import debug
 
-from . import MANIFEST
-from . import PACKAGE_DIR
+from . import MANIFEST, PACKAGE_DIR
 
 
 def validate_command(cmd: str):
@@ -17,8 +15,7 @@ def validate_command(cmd: str):
     debug(cmd)
     result = subprocess.run(  # nosec
         shlex_split(cmd),
-        stdout=PIPE,
-        stderr=PIPE,
+        capture_output=True,
     )
     # The command must always fail, since tmp_file_args includes invalid
     # arguments
@@ -47,7 +44,7 @@ def test_task_interface(task, tmp_path):
     tmp_file_args = str(tmp_path / "args.json")
     tmp_file_metadiff = str(tmp_path / "metadiff.json")
     with open(tmp_file_args, "w") as fout:
-        args = dict(wrong_arg_1=123, wrong_arg_2=[1, 2, 3])
+        args = {"wrong_arg_1": 123, "wrong_arg_2": [1, 2, 3]}
         json.dump(args, fout, indent=4)
 
     for key in ["executable_non_parallel", "executable_parallel"]:
