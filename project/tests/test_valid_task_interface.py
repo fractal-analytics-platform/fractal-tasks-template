5a452ed79c702d56859455049703a9175f650609
lorenzo <lorenzo.cerrone@uzh.ch>
2024-08-15 16:54:00 +0200

    update test to pydantic v2 errors

--- project/tests/test_valid_task_interface.py
+++ project/tests/test_valid_task_interface.py
@@ -26,13 +26,13 @@ def validate_command(cmd: str):
     stderr = result.stderr.decode()
     debug(stderr)
 
-    # Valid stderr includes pydantic.error_wrappers.ValidationError (type
+    # Valid stderr includes pydantic.v1.error_wrappers.ValidationError (type
     # match between model and function, but tmp_file_args has wrong arguments)
-    assert "pydantic.v1.error_wrappers.ValidationError" in stderr
+    assert "ValidationError" in stderr
 
-    # Valid stderr must include a mention of "unexpected keyword arguments",
+    # Valid stderr must include a mention of "Unexpected keyword argument",
     # because we are including some invalid arguments
-    assert "unexpected keyword arguments" in stderr
+    assert "Unexpected keyword argument" in stderr
 
     # Invalid stderr includes ValueError
     assert "ValueError" not in stderr
