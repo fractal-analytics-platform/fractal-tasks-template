b5688442ac8e153159818d39944d1490fb2378bf
lorenzo <lorenzo.cerrone@uzh.ch>
2025-09-22 10:00:27 +0200

    rm superfluous tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,9 +3,7 @@ from pathlib import Path
 
 import fractal_tasks_template
 
-PACKAGE = "fractal_tasks_template"
 PACKAGE_DIR = Path(fractal_tasks_template.__file__).parent
 MANIFEST_FILE = PACKAGE_DIR / "__FRACTAL_MANIFEST__.json"
 with MANIFEST_FILE.open("r") as f:
     MANIFEST = json.load(f)
-    TASK_LIST = MANIFEST["task_list"]
