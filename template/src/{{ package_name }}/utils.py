a1f46f3cc08f44388d122056b6d35d780cd99f00
lorenzo <lorenzo.cerrone@uzh.ch>
2026-03-17 14:59:20 +0100

    update template

--- template/src/{{ package_name }}/utils.py
+++ template/src/{{ package_name }}/utils.py
@@ -1,36 +1,38 @@
 """Pydantic models for advanced iterator configuration."""
 
-from typing import Literal, Optional
+from typing import Literal
 
 from pydantic import BaseModel, Field
 
 
 class MaskingConfiguration(BaseModel):
-    """Masking configuration.
-
-    Args:
-        mode (Literal["Table Name", "Label Name"]): Mode of masking to be applied.
-            If "Table Name", the identifier refers to a masking table name.
-            If "Label Name", the identifier refers to a label image name.
-        identifier (str): Name of the masking table or label image
-            depending on the mode.
-    """
+    """Masking configuration."""
 
     mode: Literal["Table Name", "Label Name"] = "Table Name"
-    identifier: Optional[str] = None
+    """
+    Mode of masking to be applied. If "Table Name", the identifier refers to a
+    masking table name.
+    If "Table Name", the identifier refers to a masking table name.
+    If "Label Name", the identifier refers to a label image name.
+    """
+    identifier: str | None = None
+    """
+    Name of the masking table or label image depending on the mode.
+    """
 
 
 class IteratorConfiguration(BaseModel):
-    """Advanced Masking configuration.
+    """Advanced Masking configuration."""
 
-    Args:
-        masking (Optional[MaskingIterator]): If configured, the segmentation
-            will be only saved within the mask region.
-        roi_table (Optional[str]): Name of a ROI table. If provided, the segmentation
-            will be performed for each ROI in the specified ROI table.
-    """
-
-    masking: Optional[MaskingConfiguration] = Field(
+    masking: MaskingConfiguration | None = Field(
         default=None, title="Masking Iterator Configuration"
     )
-    roi_table: Optional[str] = Field(default=None, title="Iterate Over ROIs")
+    """
+    If configured, the segmentation will be only saved within the mask region.
+    will be applied based on the provided masking configuration.
+    """
+    roi_table: str | None = Field(default=None, title="Iterate Over ROIs")
+    """
+    Name of a ROI table. If provided, the segmentation will be performed for each ROI
+    in the specified ROI table.
+    """
